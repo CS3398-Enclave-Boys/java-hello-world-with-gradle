582106f90aa5fc4fe71d2dad43adcfc66ad0697a
les126 <les126@txstate.edu>
2019-10-01 19:48:13 -0500

    Update TestGreeter.java

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -7,32 +7,32 @@ import org.junit.Test;
 public class TestGreeter {
    private Greeter g;
    @Before
-   public void setUp() throws Exception 
+   public void setUp() throws Exception
    {
       g = new Greeter();
    }
 
 
    @Test
-   public void testGreeterEmpty() 
+   public void testGreeterEmpty()
    {
       assertEquals(g.getName(),"");
       assertEquals(g.sayHello(),"Hello!");
    }
 
    @Test
-   public void testGreeter() 
+   public void testGreeter()
    {
       g.setName("World");
       assertEquals(g.getName(),"World");
       assertEquals(g.sayHello(),"Hello World!");
    }
-   
+
    @Test
-   public void newtestWMGreeterPass() {
-       g.setName("Boris");
-       assertEquals(g.getName(),"Boris");
-       assertEquals(g.sayHello(),"Hello Boris!");
+   public void newtestLSGreeterPass() {
+       g.setName("Tom");
+       assertEquals(g.getName(),"Tom");
+       assertEquals(g.sayHello(),"Hello Tom!");
    }
    /*
    @Test
@@ -42,5 +42,4 @@ public class TestGreeter {
        assertEquals(g.sayHello(),"Hello Boris!");
    }
    */
-   
 }
